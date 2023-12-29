dd4d1e99fa3636835448bc7826f2644e1e3b55fe
Никита <steaveramires@gmail.com>
2023-12-29 20:08:51 +0300

    serverside ragdoll
    Player ragdolls are now server-side ( On Dobrograd this is already there, but for the addon it's necessary, otherwise I can't implement to make it work for players )

--- putrefaction/lua/autorun/sh_crunchydecay.lua
+++ putrefaction/lua/autorun/sh_crunchydecay.lua
@@ -56,3 +56,101 @@ if CLIENT then
         )
     end
 end
+
+--- 
+
+if CLIENT then return end
+AddCSLuaFile("cl_serversideragdolls.lua")
+util.AddNetworkString("ragColor")
+local meta = FindMetaTable("Player")
+if not meta then return end
+if not meta.CreateRagdollOld then meta.CreateRagdollOld = meta.CreateRagdoll end
+local EnableServerSideRagdolls = CreateConVar("sv_ragdolls", "1", {FCVAR_REPLICATED, FCVAR_NOTIFY}, "Enable server side player corpse ragdolls.")
+local RagdollSpeedMultiplier = CreateConVar("sv_ragdolls_speed", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Speed multiplier of player corpse ragdolls.")
+local RagdollWeightMultiplier = CreateConVar("sv_ragdolls_weight", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Weight multiplier of player corpse ragdolls.")
+local RagdollsCollideWithPlayers = CreateConVar("sv_ragdolls_collide_players", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE}, "Whether player corpses should collide with players or not.")
+function meta:CreateRagdoll()
+    if not EnableServerSideRagdolls:GetBool() then
+        self:CreateRagdollOld()
+        return
+    end
+
+    if not (self or self:IsValid() or self:IsPlayer()) then return end
+    self.DeathRagdoll = self.DeathRagdoll or false
+    if self.DeathRagdoll then if self.DeathRagdoll:IsValid() then self.DeathRagdoll:Remove() end end
+    local ply_pos = self:GetPos()
+    local ply_ang = self:GetAngles()
+    local ply_mdl = self:GetModel()
+    local ply_skn = self:GetSkin()
+    local ply_col = self:GetColor()
+    local ply_mat = self:GetMaterial()
+    local playerModelIsRagdoll = self:GetBoneCount() > 1
+    local ent
+    if playerModelIsRagdoll then
+        ent = ents.Create("prop_ragdoll")
+    else
+        ent = ents.Create("prop_physics")
+    end
+
+    ent:SetPos(ply_pos)
+    ent:SetAngles(ply_ang - Angle(ply_ang.p, 0, 0))
+    ent:SetModel(ply_mdl)
+    ent:SetSkin(ply_skn)
+    ent:SetColor(ply_col)
+    ent:SetMaterial(ply_mat)
+    ent:SetCreator(self)
+    for k, v in ipairs(self:GetBodyGroups()) do
+        --MsgN("Setting " .. self:GetBodygroupName(v.id).. " to " .. self:GetBodygroup(v.id))
+        ent:SetBodygroup(v.id, self:GetBodygroup(v.id))
+    end
+
+    ent:Spawn()
+    local phys = ent:GetPhysicsObject()
+    if phys:IsValid() then phys:SetMass(phys:GetMass() * RagdollWeightMultiplier:GetFloat()) end
+    if not ent:IsValid() then return end
+    self.DeathRagdoll = ent
+    local plyvel = self:GetVelocity()
+    if playerModelIsRagdoll then
+        -- Position and rotate the ragdoll's limbs, set their velocity
+        for i = 0, ent:GetPhysicsObjectCount() - 1 do
+            local bone = ent:GetPhysicsObjectNum(i)
+            if bone and bone:IsValid() then
+                local bonepos, boneang = self:GetBonePosition(ent:TranslatePhysBoneToBone(i))
+                bone:SetPos(bonepos)
+                bone:SetAngles(boneang)
+                bone:SetVelocity(plyvel * RagdollSpeedMultiplier:GetFloat())
+            end
+        end
+
+        -- Pose the ragdoll's face
+        ent:SetFlexScale(self:GetFlexScale())
+        for i = 1, ent:GetFlexNum() do
+            ent:SetFlexWeight(i, self:GetFlexWeight(i))
+        end
+
+        -- Set the ragdoll's playermodel color
+        local ply_ragcol = self:GetPlayerColor()
+        ent.RagColor = Vector(ply_ragcol.r, ply_ragcol.g, ply_ragcol.b)
+        net.Start("ragColor")
+        net.WriteInt(ent:EntIndex(), 16)
+        net.WriteVector(ent.RagColor)
+        net.Broadcast()
+    else
+        phys:SetVelocity(plyvel * RagdollSpeedMultiplier:GetFloat())
+    end
+
+    ent:SetCreator(self)
+    if self:IsOnFire() then ent:Ignite(math.Rand(6, 8), 0) end
+    if not RagdollsCollideWithPlayers:GetBool() then ent:SetCollisionGroup(COLLISION_GROUP_WEAPON) end
+    self:SpectateEntity(ent)
+    self:Spectate(OBS_MODE_CHASE)
+end
+
+if SERVER then return end
+
+net.Receive("ragColor", function(len)
+	local rag = Entity(net.ReadInt(16))
+	local col = net.ReadVector()
+
+	rag.GetPlayerColor = function() return col end
+end)
